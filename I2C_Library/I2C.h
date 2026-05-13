fe9b564690fd8d956354a6fedf609cf330a38a01
RUN远方 <arounyf@gmail.com>
2026-05-13 06:12:38 +0000

    Restore original I2C driver, add OEC DTB with optimizations
    - Revert I2C.c/h to original CumeBox2 kernel I2C implementation
    - Remove libgpiod dependency from Makefile
    - Add patched DTB for OEC Turbo: i2c-gpio@500kHz, fiq-debugger disabled
    - Include original DTB for reference

--- I2C_Library/I2C.h
+++ I2C_Library/I2C.h
@@ -1,32 +1,54 @@
 /*
- * I2C.h — Linux kernel I2C character device interface
- */
+ * MIT License
+
+Copyright (c) 2017 DeeplyEmbedded
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ * I2C.h
+ *
+ *  Created on  : Sep 4, 2017
+ *  Author      : Vinay Divakar
+ *  Website     : www.deeplyembedded.org
+ */ 
 
 #ifndef I2C_H_
 #define I2C_H_
 
-#include <stdint.h>
-#include <stdbool.h>
-#include <linux/i2c-dev.h>
+#include<stdint.h>
 
 /* No. of bytes per transaction */
 #define I2C_ONE_BYTE                     1
 #define I2C_TWO_BYTES                    2
 #define I2C_THREE_BYTES                  3
 
-/* I2C device path (kept for API compat, ignored in bit-bang mode) */
-#define I2C_DEV0_PATH                    "/dev/i2c-0"
-#define I2C_DEV1_PATH                    "/dev/i2c-1"
-#define I2C_DEV2_PATH                    "/dev/i2c-2"
+/*Definitions specific to i2c-x */
+#define I2C_DEV0_PATH                     "/dev/i2c-0"
+#define I2C_DEV1_PATH                     "/dev/i2c-1"
+#define I2C_DEV2_PATH                     "/dev/i2c-2"
 
-/* I2C device configuration structure */
-typedef struct {
-    char *i2c_dev_path;
-    int fd_i2c;
-    unsigned char i2c_slave_addr;
-} I2C_DeviceT, *I2C_DevicePtr;
-
-extern I2C_DeviceT I2C_DEV_2;
+/*I2C device configuration structure*/
+typedef struct{
+	char* i2c_dev_path;
+	int fd_i2c;
+	unsigned char i2c_slave_addr;
+}I2C_DeviceT, *I2C_DevicePtr;
 
 /* Exposed Generic I2C Functions */
 extern int Open_device(char *i2c_dev_path, int *fd);
@@ -35,10 +57,13 @@ extern int Set_slave_addr(int fd, unsigned char slave_addr);
 extern int i2c_write(int fd, unsigned char data);
 extern int i2c_read(int fd, unsigned char *read_data);
 extern int i2c_read_register(int fd, unsigned char read_addr, unsigned char *read_data);
-extern int i2c_read_registers(int fd, int num, unsigned char starting_addr, unsigned char *buff_Ptr);
+extern int i2c_read_registers(int fd, int num, unsigned char starting_addr,
+		unsigned char *buff_Ptr);
 extern void config_i2c_struct(char *i2c_dev_path, unsigned char slave_addr, I2C_DevicePtr i2c_dev);
 extern int i2c_multiple_writes(int fd, int num, unsigned char *Ptr_buff);
 extern int i2c_write_register(int fd, unsigned char reg_addr_or_cntrl, unsigned char val);
-extern int init_i2c_dev(const char *i2c_path, unsigned char slave_address);
+
+/* Exposed I2C-x Specific Functions */
+extern int init_i2c_dev(const char* i2c_path, unsigned char slave_address);
 
 #endif /* I2C_H_ */
--- Makefile
+++ Makefile
@@ -3,12 +3,12 @@ LD = gcc
 
 SOURCES = SSD1306_OLED_Library/SSD1306_OLED.c Main/Main.c Main/dataapi.c I2C_Library/I2C.c cJSON/cJSON.c
 OBJS := $(SOURCES:.c=.o)
-CPPFLAGS := -I SSD1306_OLED_Library -I I2C_Library -I cJSON -I Main
-CFLAGS := -g -Wall
-LDFLAGS :=
+CPPFLAGS := -I SSD1306_OLED_Library -I I2C_Library -I cJSON
+CFLAGS := -g
 
 ssd: $(OBJS)
-	$(CC) $^ -o $@ $(LDFLAGS)
+	$(CC) $^ -o $@
 
 clean:
 	rm -rf ssd $(OBJS)
+
